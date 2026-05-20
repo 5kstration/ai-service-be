3bae4a747003cf0d80936ac25df8a50b76a3d92f
서지혜 <okcoco0390@gmail.com>
2026-05-20 01:49:16 -0400

    refactor: 전체적인구조 수정

--- app/domain/insight/entity.py
+++ app/domain/insight/entity.py
@@ -1,28 +1,37 @@
 # app/domain/insight/entity.py
 import enum
-from sqlalchemy import Column, String, Text, DateTime, Enum
+from sqlalchemy import Column, String, Integer, BigInteger, Text, DateTime, Enum
 from sqlalchemy.sql import func
 from app.core.config.database import Base
 from app.core.utils.tsid import TSID
 
 
 class InsightType(str, enum.Enum):
-    MONTHLY_ANALYSIS  = "월별 분석"
-    GOAL_SUGGESTION   = "목표 제안"
-    CARD_RECOMMEND    = "카드 추천"
+    MONTHLY_ANALYSIS    = "월별 분석"
+    GOAL_SUGGESTION     = "목표 제안"
+    CARD_RECOMMEND      = "카드 추천"
     INSURANCE_RECOMMEND = "보험 추천"
-    POLICY_RECOMMEND  = "청년 정책 추천"
+    POLICY_RECOMMEND    = "청년 정책 추천"
 
 
 class AiInsight(Base):
     __tablename__ = "ai_insight"
 
-    insight_id   = Column(String(26), primary_key=True, default=TSID.create)
-    user_id      = Column(String(26),  nullable=False)
-    insight_type = Column(Enum(InsightType, name="insight_type_enum"),nullable=False)
-    title        = Column(String(255))
-    description  = Column(Text)
-    icon_type    = Column(String(50))
-    accent_color = Column(String(10))
-    goal_id      = Column(String(26))
-    created_at   = Column(DateTime, server_default=func.now())
+    insight_id       = Column(String(26),  primary_key=True, default=TSID.create, nullable=False)
+    user_id          = Column(String(26),  nullable=False)
+    year             = Column(Integer,     )
+    month            = Column(Integer,     )
+    insight_type     = Column(Enum(InsightType, name="insight_type_enum"), nullable=False)
+    insight_title    = Column(String(255) )
+    description      = Column(Text        )
+    target_id        = Column(String(26)  )
+    total_expense    = Column(BigInteger  )
+    goal_expense     = Column(BigInteger  )
+    achievement_rate = Column(Integer     )
+    remain_budge     = Column(Integer     )
+    remain_days      = Column(Integer     )
+    daily_budge      = Column(Integer     )
+    saving_tip       = Column(String(255) )
+    icon_type        = Column(String(50)  )
+    accent_color     = Column(String(10)  )
+    created_at       = Column(DateTime,    nullable=False, server_default=func.now())
--- app/domain/recommend/entity.py
+++ app/domain/recommend/entity.py
@@ -1,63 +1,67 @@
+# app/domain/recommend/entity.py
 from sqlalchemy import Column, String, Text, DateTime, Boolean, ForeignKey
 from sqlalchemy.sql import func
 from app.core.config.database import Base
-from app.core.utils.tsid import TSID # tsid 생성 위해 추가 > core/utils/tsid.py 참조
+from app.core.utils.tsid import TSID
 
 
 class RecommendCard(Base):
     __tablename__ = "recommend_card"
 
-    rec_card_id = Column(String(26), primary_key=True, default=TSID.create)
-    user_id         = Column(String(26), nullable=False)
+    rec_card_id     = Column(String(26),  primary_key=True, default=TSID.create, nullable=False)
+    user_id         = Column(String(26),  nullable=False)
     card_company    = Column(String(255))
     card_name       = Column(String(255))
     core_benefit    = Column(Text)
     benefit_details = Column(Text)
     ai_reason       = Column(Text)
     card_url        = Column(Text)
-    created_at      = Column(DateTime, server_default=func.now())
-    insight_id      = Column(String(26), ForeignKey("ai_insight.insight_id"))
+    category        = Column(String(50))
+    created_at      = Column(DateTime,    nullable=False, server_default=func.now())
+    insight_id      = Column(String(26),  ForeignKey("ai_insight.insight_id"), nullable=False)
 
 
 class RecommendInsurance(Base):
     __tablename__ = "recommend_insurance"
 
-    rec_ins_id       = Column(String(26), primary_key=True,default=TSID.create)
-    user_id          = Column(String(26), nullable=False)
+    rec_ins_id       = Column(String(26),  primary_key=True, default=TSID.create, nullable=False)
+    user_id          = Column(String(26),  nullable=False)
     insurance_corp   = Column(String(255))
     insurance_name   = Column(String(255))
     core_benefit     = Column(String(255))
-    coverage_details = Column(Text)
-    benefit_details  = Column(Text)
-    ai_reason        = Column(Text)
-    insurance_url    = Column(Text)
-    created_at       = Column(DateTime, server_default=func.now())
-    insight_id       = Column(String(26), ForeignKey("ai_insight.insight_id"))
+    coverage_details = Column(Text        )
+    benefit_details  = Column(Text        )
+    ai_reason        = Column(Text        )
+    insurance_url    = Column(Text        )
+    category         = Column(String(50),  )
+    created_at       = Column(DateTime,    nullable=False, server_default=func.now())
+    insight_id       = Column(String(26),  ForeignKey("ai_insight.insight_id"), nullable=False)
 
 
 class RecommendPolicy(Base):
     __tablename__ = "recommend_policy"
 
-    rec_pol_id    = Column(String(26), primary_key=True,default=TSID.create)
-    user_id       = Column(String(26), nullable=False)
+    rec_pol_id    = Column(String(26),  primary_key=True, default=TSID.create, nullable=False)
+    user_id       = Column(String(26),  nullable=False)
     policy_org    = Column(String(255))
     policy_name   = Column(String(255))
     core_benefit  = Column(String(255))
     policy_detail = Column(Text)
     ai_reason     = Column(Text)
     policy_url    = Column(Text)
-    created_at    = Column(DateTime, server_default=func.now())
-    insight_id    = Column(String(26), ForeignKey("ai_insight.insight_id"))
+    category      = Column(String(50)  )
+    created_at    = Column(DateTime,    nullable=False, server_default=func.now())
+    insight_id    = Column(String(26),  ForeignKey("ai_insight.insight_id"), nullable=False)
 
 
 class Bookmark(Base):
     __tablename__ = "bookmark"
 
-    bookmark_id   = Column(String(26), primary_key=True, default=TSID.create)
+    bookmark_id   = Column(String(26), primary_key=True, default=TSID.create, nullable=False)
     user_id       = Column(String(26), nullable=False)
     target_type   = Column(String(20), nullable=False)
     target_ref_id = Column(String(26), nullable=False)
     remind_at     = Column(DateTime)
-    is_reminded   = Column(Boolean, default=False)
-    created_at    = Column(DateTime, server_default=func.now())
+    is_reminded   = Column(Boolean,    nullable=False, default=False)
+    created_at    = Column(DateTime,   nullable=False, server_default=func.now())
     deleted_at    = Column(DateTime)
